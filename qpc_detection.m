ac7cf479fb61b7bd5b3a0ed616582c73698abf52
sotirios <sotiriosmos@gmail.com>
2021-07-11 16:23:01 +0300

    some question 3 improvements

--- qpc_detection.m
+++ qpc_detection.m
@@ -8,7 +8,7 @@ fftsignal = (fft(signal));
 n = length(fftsignal);   % number of samples
 f = (0:n-1)*fs/n;  % frequency range
 power = abs(fftsignal).^2/n;  % power of the DFT
-power = (power/max(power)); % To normalize the values
+power = (power/max(power));   % normalize the values
 
 figure;
 plot(f,power)
@@ -28,16 +28,50 @@ signal = [signal ; z];
 Y = reshape(signal,M,K);
 
 Nfft = 128;
+
 figure;
 [bspec,waxis] = bispecd(Y,Nfft,0,K);
-waxis1 = waxis*fs;
+SS = Nfft/2+1;
+waxis1 = waxis(SS:end)*fs;
 figure;
-contour(waxis1,waxis1,abs(bspec));
+subplot(121)
+bspec1 = bspec(SS:end,SS:end);
+contour(waxis1,waxis1,abs(bspec1),10);
 set(gca,'ylim',[-20 20])
 set(gca,'xlim',[-20 20])
 grid on
-title('bispectrum')
+title('Bispectrum')
 xlabel('f1'),ylabel('f2')
+
+% Bispectrum symmetries
+
+hline1 = refline(0, 0); 
+hline1.Color = 'k';
+hline2 = refline(-1, 0.5);
+hline2.Color = 'k';
+hline3 = refline(1, 0);
+hline3.Color = 'k';
+
+[X1,Y1] = meshgrid(waxis1,waxis1);
+subplot(122)
+mesh(X1,Y1,abs(bspec1))
+axis tight;
+set(gca,'ylim',[-20 20])
+set(gca,'xlim',[-20 20])
+title('Bispectrum')
+ylabel('f1')
+ylabel('f2')
+zlabel('range')
+
+% Bispectrum symmetries
+
+hline1 = refline(0, 0); 
+hline1.Color = 'k';
+hline2 = refline(-1, 0.5);
+hline2.Color = 'k';
+hline3 = refline(1, 0);
+hline3.Color = 'k';
+
 
 end
 
